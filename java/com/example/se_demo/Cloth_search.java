dd1049adb45cc01034b7b046982eeb70a03f3a79
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-11-22 20:49:01 +0900

    figma 디자인 적용(drawer화면, search화면)

--- java/com/example/se_demo/Cloth_search.java
+++ java/com/example/se_demo/Cloth_search.java
@@ -1,6 +1,9 @@
 package com.example.se_demo;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +13,10 @@ public class Cloth_search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cloth_search);
 
-
+        Button search_top = (Button)findViewById(R.id.search_top);
+        Button search_bottom = (Button)findViewById(R.id.search_bottom);
+        Button search_long = (Button)findViewById(R.id.search_long);
+        Button search_short = (Button)findViewById(R.id.search_short);
 
     }
 }
